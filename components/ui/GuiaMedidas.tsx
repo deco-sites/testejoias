ff16e38be90b9a962c98c29c05a9e8df58017bf9
Matheus Macedo <matheus.macedo@agenciagh.com.br>
2024-03-27 17:08:19 -0300

    ajuste

--- components/ui/GuiaMedidas.tsx
+++ components/ui/GuiaMedidas.tsx
@@ -21,9 +21,6 @@ export interface Props {
 }
 
 
-let rangeValue = 50;
-
-
 function GuiaMedidas(
     {
         tabela1,
@@ -34,20 +31,20 @@ function GuiaMedidas(
 
 
     return (
-        <div class="container lg:pb-28 flex justify-between">
+        <div class="container lg:pb-28 flex justify-between flex-wrap px-4 pb-12"> 
 
             <div class="tabela">
                 <div class="relative">
-                    <div class="table">
-                        <Icon id="QuadroMedidas" class="bg-[#FFFFFF]" width={536} height={536} />
+                    <div class="table"> 
+                        <Icon id="QuadroMedidas" class="bg-[#FFFFFF] max-w-full" width={536} height={536} />
                     </div>
                     <div class="medida absolute top-0 bottom-0 left-0 right-0 m-auto w-10 h-10 rounded-full bg-[#2B3243]"></div>
                 </div> 
-            </div>
+            </div> 
 
             <div class="informacoes lg:w-[520px]">
 
-                <div class="flex">
+                <div class="flex tabela-medida">
 
                     <div class="content">
 
@@ -58,16 +55,16 @@ function GuiaMedidas(
                             <div class="diametro w-32">
                                 <b>Diâmetro</b>
                             </div>
-                        </div>
+                        </div> 
 
                         <div class="conteudo">
                             {tabela1 && tabela1.length > 0 && (
                                 tabela1.map((item) => (
                                     <div class="flex">
-                                        <div class="aro w-28 border-solid border-base-300 lg:border-t py-1 px-1">
+                                        <div class="aro w-28 border-solid border-base-300 border-t py-1 px-1">
                                             <span>{item.aro}</span>
                                         </div>
-                                        <div class="diametro w-32 border-solid border-base-300 lg:border-t py-1 lg:border-r">
+                                        <div class="diametro w-32 border-solid border-base-300 border-t py-1 border-r">
                                             <span>{item.diametro}</span>
                                         </div>
                                     </div>
@@ -91,10 +88,10 @@ function GuiaMedidas(
                             {tabela2 && tabela2.length > 0 && (
                                 tabela2.map((item) => (
                                     <div class="flex">
-                                        <div class="aro w-28 border-solid border-base-300 lg:border-t py-1 px-1 pl-4 ">
+                                        <div class="aro w-28 border-solid border-base-300 border-t py-1 px-1 pl-4 ">
                                             <span>{item.aro}</span>
                                         </div>
-                                        <div class="diametro w-32 border-solid border-base-300 lg:border-t py-1">
+                                        <div class="diametro w-32 border-solid border-base-300 border-t py-1">
                                             <span>{item.diametro}</span>
                                         </div>
                                     </div>
